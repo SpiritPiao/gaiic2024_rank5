748a5d74898aaab0283d144754fbb1feac170492
nowandfuture <1183424791@qq.com>
2024-07-09 08:28:47 +0000

    Update README

--- mmdet/models/backbones/vision_transformer.py
+++ mmdet/models/backbones/vision_transformer.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from functools import partial
 from typing import Sequence
+import warnings
 
 import numpy as np
 import torch
@@ -8,114 +9,15 @@ import torch.nn as nn
 from mmcv.cnn.bricks.transformer import FFN, PatchEmbed
 from mmengine.model import BaseModule, ModuleList
 from mmengine.model.weight_init import trunc_normal_
+from mmcv.cnn.bricks.drop import build_dropout
 
 from mmdet.registry import MODELS
-from ..utils import (MultiheadAttention, SwiGLUFFNFused, build_norm_layer,
-                     resize_pos_embed, to_2tuple)
+# from ..utils import (MultiheadAttention, SwiGLUFFNFused, build_norm_layer,
+#                      resize_pos_embed, to_2tuple)
 # from .base_backbone import BaseBackbone
 from mmengine.model import BaseModule, ModuleList
-
-
-
-class MultiheadAttention(BaseModule):
-    """Multi-head Attention Module.
-
-    This module implements multi-head attention that supports different input
-    dims and embed dims. And it also supports a shortcut from ``value``, which
-    is useful if input dims is not the same with embed dims.
-
-    Args:
-        embed_dims (int): The embedding dimension.
-        num_heads (int): Parallel attention heads.
-        input_dims (int, optional): The input dimension, and if None,
-            use ``embed_dims``. Defaults to None.
-        attn_drop (float): Dropout rate of the dropout layer after the
-            attention calculation of query and key. Defaults to 0.
-        proj_drop (float): Dropout rate of the dropout layer after the
-            output projection. Defaults to 0.
-        dropout_layer (dict): The dropout config before adding the shortcut.
-            Defaults to ``dict(type='Dropout', drop_prob=0.)``.
-        qkv_bias (bool): If True, add a learnable bias to q, k, v.
-            Defaults to True.
-        qk_scale (float, optional): Override default qk scale of
-            ``head_dim ** -0.5`` if set. Defaults to None.
-        proj_bias (bool) If True, add a learnable bias to output projection.
-            Defaults to True.
-        v_shortcut (bool): Add a shortcut from value to output. It's usually
-            used if ``input_dims`` is different from ``embed_dims``.
-            Defaults to False.
-        use_layer_scale (bool): Whether to use layer scale. Defaults to False.
-        layer_scale_init_value (float or torch.Tensor): Init value of layer
-            scale. Defaults to 0.
-        init_cfg (dict, optional): The Config for initialization.
-            Defaults to None.
-    """
-
-    def __init__(self,
-                 embed_dims,
-                 num_heads,
-                 input_dims=None,
-                 attn_drop=0.,
-                 proj_drop=0.,
-                 dropout_layer=dict(type='Dropout', drop_prob=0.),
-                 qkv_bias=True,
-                 qk_scale=None,
-                 proj_bias=True,
-                 v_shortcut=False,
-                 use_layer_scale=False,
-                 layer_scale_init_value=0.,
-                 init_cfg=None):
-        super(MultiheadAttention, self).__init__(init_cfg=init_cfg)
-
-        self.input_dims = input_dims or embed_dims
-        self.embed_dims = embed_dims
-        self.num_heads = num_heads
-        self.v_shortcut = v_shortcut
-
-        self.head_dims = embed_dims // num_heads
-        if qk_scale is not None:
-            self.scaled_dot_product_attention = partial(
-                scaled_dot_product_attention_pyimpl,
-                scale=self.head_dims**-0.5)
-        else:
-            self.scaled_dot_product_attention = scaled_dot_product_attention
-
-        self.qkv = nn.Linear(self.input_dims, embed_dims * 3, bias=qkv_bias)
-        self.attn_drop = attn_drop
-        self.proj = nn.Linear(embed_dims, embed_dims, bias=proj_bias)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-        self.out_drop = build_dropout(dropout_layer)
-
-        if use_layer_scale:
-            warnings.warn('The `use_layer_scale` in `MultiheadAttention` will '
-                          'be deprecated. Please use `layer_scale_init_value` '
-                          'to control whether using layer scale or not.')
-
-        if use_layer_scale or (layer_scale_init_value > 0):
-            layer_scale_init_value = layer_scale_init_value or 1e-5
-            self.gamma1 = LayerScale(
-                embed_dims, layer_scale_init_value=layer_scale_init_value)
-        else:
-            self.gamma1 = nn.Identity()
-
-    def forward(self, x):
-        B, N, _ = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads,
-                                  self.head_dims).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]
-
-        attn_drop = self.attn_drop if self.training else 0.
-        x = self.scaled_dot_product_attention(q, k, v, dropout_p=attn_drop)
-        x = x.transpose(1, 2).reshape(B, N, self.embed_dims)
-
-        x = self.proj(x)
-        x = self.out_drop(self.gamma1(self.proj_drop(x)))
-
-        if self.v_shortcut:
-            x = v.squeeze(1) + x
-        return x
-
+from ..utils.attention import MultiheadAttention, LayerScale, SwiGLUFFN, SwiGLUFFNFused, to_2tuple, resize_pos_embed
+from ..utils.norm import build_norm_layer
 
 
 class TransformerEncoderLayer(BaseModule):
